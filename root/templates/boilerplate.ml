71922e7cfd8396f63863b81202fa1aeb091cea1a
Captain Nick Lucifer* <naryal2580@gmail.com>
2021-09-17 08:49:41 +0545

    added local fonts

--- root/templates/boilerplate.ml
+++ root/templates/boilerplate.ml
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" /> <!-- TODO: Localizing the icons. -->
+    <link rel="stylesheet" href="/css/font-awesome.css" type="text/css" />
     <link rel="stylesheet" href="/css/mui.min.css" type="text/css" />
     <link rel="stylesheet" href="/css/style.css" type="text/css" />
     <title>{{ page.title }}</title>
